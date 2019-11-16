8ecde4a3c865854aa9055e35ddffeb7934cf108f
chirags <chirags@cybage.com>
2019-11-15 22:09:07 -0600

    commiting changes.

--- src/main/java/com/example/pyramid/controller/PyramidController.java
+++ src/main/java/com/example/pyramid/controller/PyramidController.java
@@ -1,22 +1,18 @@
 package com.example.pyramid.controller;
 
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
